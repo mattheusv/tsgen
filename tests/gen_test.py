9d9f07a39163d0bbc4df10f3b20e9693bfe72275
Matheus Alcantara <matheusssilv97@gmail.com>
2019-07-11 22:15:37 -0300

    test: improvement initial tests

--- tests/gen_test.py
+++ tests/gen_test.py
@@ -1,39 +1,38 @@
 import pandas as pd
 import numpy as np
+import os.path
 from unittest import TestCase
 from tsgen.gen import TimeSerieGenerator
-from tsgen.exceptions import InvalidValue
 
 
 class TimeSerieGeneratorTestCase(TestCase):
-    def test_timeserie_generator_valid(self):
-        try:
-            TimeSerieGenerator(
-                date_start="01-01-1990",
-                date_end=pd.Timestamp.now(),
-                freq="D",
-                tz="UTC",
-                low=0,
-                high=100,
-                dtype=np.int,
-                ts_name="ts.test",
-            )
-        except InvalidValue as e:
-            self.failIf("unknown timeserie generator invalid value exception", e)
+    def test_generate_df_freq(self):
+        ts_gen = TimeSerieGenerator(
+            date_start="1990-01-01",
+            date_end="1990-01-02",
+            freq="D",
+            tz="UTC",
+            low=0,
+            high=100,
+            ts_name="ts.test",
+        )
+        df = ts_gen.generate_df()
+        self.assertEqual(len(df), 2)
 
-    def test_generate_df(self):
-        try:
-            ts_gen = TimeSerieGenerator(
-                date_start="1990-01-01",
-                date_end=pd.Timestamp.now(),
-                freq="D",
-                tz="UTC",
-                low=0,
-                high=100,
-                dtype=np.int,
-                ts_name="ts.test",
+    def test_export_df(self):
+        ts_gen = TimeSerieGenerator(
+            date_start="1990-01-01",
+            date_end="1990-01-02",
+            freq="D",
+            tz="UTC",
+            low=0,
+            high=100,
+            ts_name="ts.test",
+        )
+        df = ts_gen.generate_df()
+        ts_gen.export_df(df)
+        self.assertTrue(
+            os.path.exists(
+                f"{ts_gen.ts_name}_{pd.datetime.now().strftime('%y-%m-%d')}.csv"
             )
-            df = ts_gen.generate_df()
-            print(df.head())
-        except Exception as e:
-            self.failIf("unknown exception", e)
+        )
