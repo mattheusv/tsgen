2b73190d0aa6dd11568210220785cdb533cf78f3
Matheus Alcantara <matheusssilv97@gmail.com>
2019-07-13 12:49:18 -0300

    test: create test to TimeSeriesGenerator.generate

--- tests/gen_test.py
+++ tests/gen_test.py
@@ -27,7 +27,7 @@ class TimeSerieGeneratorTestCase(TestCase):
             tz="UTC",
             low=0,
             high=100,
-            ts_name="ts.test",
+            ts_name="ts.test_export_df",
         )
         df = ts_gen.generate_df()
         ts_gen.export_df(df)
@@ -36,3 +36,21 @@ class TimeSerieGeneratorTestCase(TestCase):
                 f"{ts_gen.ts_name}_{pd.datetime.now().strftime('%y-%m-%d')}.csv"
             )
         )
+
+    def test_export(self):
+        ts_gen = TimeSerieGenerator(
+            date_start="1990-01-01",
+            date_end="1990-01-02",
+            freq="D",
+            tz="UTC",
+            low=0,
+            high=100,
+            ts_name="ts.test_export",
+        )
+        ts_gen.generate()
+        self.assertTrue(
+            os.path.exists(
+                f"{ts_gen.ts_name}_{pd.datetime.now().strftime('%y-%m-%d')}.csv"
+            )
+        )
+
